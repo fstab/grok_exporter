d33f356cdefd256859d2a96e00da76ab8adb5de4
Fabian Stäber <fabian@fstab.de>
2016-07-24 10:34:25 +0200

    metrics for bufferedTailer

--- bufferedTailer.go
+++ bufferedTailer.go
@@ -6,6 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"log"
 	"sync"
+	"time"
 )
 
 type bufferedTailerWithMetrics struct {
@@ -32,33 +33,43 @@ func BufferedTailerWithMetrics(origTailer tailer.Tailer) tailer.Tailer {
 
 	// producer
 	go func() {
-		linesRead := prometheus.NewCounter(prometheus.CounterOpts{
-			Name: "grok_exporter_lines_read_total",
-			Help: "Number of lines that are read from the logfile, including lines that are not yet processed by the configured metrics.",
+		bufferLoad := prometheus.NewSummary(prometheus.SummaryOpts{
+			Name: "grok_exporter_line_buffer_peak_load",
+			Help: "Number of lines that are read from the logfile and waiting to be processed. Peak value per second.",
 		})
-		prometheus.MustRegister(linesRead)
-		for line := range origTailer.LineChan() {
-			bufferSync.L.Lock()
-			buffer.PushBack(line)
-			bufferSync.Signal()
-			bufferSync.L.Unlock()
-			linesRead.Inc()
+		prometheus.MustRegister(bufferLoad)
+		bufferLoadLastSecond := 0
+		tick := time.NewTicker(1 * time.Second)
+		for {
+			select {
+			case line, ok := <-origTailer.LineChan():
+				if ok {
+					bufferSync.L.Lock()
+					if buffer.Len() > bufferLoadLastSecond {
+						bufferLoadLastSecond = buffer.Len()
+					}
+					buffer.PushBack(line)
+					bufferSync.Signal()
+					bufferSync.L.Unlock()
+				} else {
+					bufferSync.L.Lock()
+					buffer = nil // make the consumer quit
+					bufferSync.Signal()
+					bufferSync.L.Unlock()
+					prometheus.Unregister(bufferLoad)
+					close(out)
+					tick.Stop()
+					return
+				}
+			case <-tick.C:
+				bufferLoad.Observe(float64(bufferLoadLastSecond))
+				bufferLoadLastSecond = 0
+			}
 		}
-		bufferSync.L.Lock()
-		buffer = nil // make the consumer quit
-		bufferSync.Signal()
-		bufferSync.L.Unlock()
-		prometheus.Unregister(linesRead)
-		close(out)
 	}()
 
 	// consumer
 	go func() {
-		linesProcessed := prometheus.NewCounter(prometheus.CounterOpts{
-			Name: "grok_exporter_lines_processed_total",
-			Help: "Number of lines that are handed over to the configured metrics for evaluation.",
-		})
-		prometheus.MustRegister(linesProcessed)
 		for {
 			bufferSync.L.Lock()
 			for buffer != nil && buffer.Len() == 0 {
@@ -66,7 +77,6 @@ func BufferedTailerWithMetrics(origTailer tailer.Tailer) tailer.Tailer {
 			}
 			if buffer == nil {
 				bufferSync.L.Unlock()
-				prometheus.Unregister(linesProcessed)
 				return
 			}
 			first := buffer.Front()
@@ -75,7 +85,6 @@ func BufferedTailerWithMetrics(origTailer tailer.Tailer) tailer.Tailer {
 			switch line := first.Value.(type) {
 			case string:
 				out <- line
-				linesProcessed.Inc()
 			default:
 				// this cannot happen
 				log.Fatal("unexpected type in tailer buffer")
