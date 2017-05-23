c1131755861724e10d8285ad01efcbfc8b144cf2
wangyaming <wangyaming@dayang.com.cn>
2017-05-23 18:28:20 +0800

    debug

--- grok_exporter.go
+++ grok_exporter.go
@@ -137,19 +137,13 @@ func pushMetric(m exporter.Metric, pushUrl string, groupingKey map[string]string
 		return err
 	}
 	//remove metric from collector
-	deleted := false
-	if m.metricVec != nil {
-		deleted = m.metricVec.DeleteLabelValues(labelValues...)
-	}
+	deleted := m.Collector().DeleteLabelValues(labelValues...)
 
 	if deleted {
 		fmt.Println(fmt.Sprintf("[DEBUG] Deleted metric %s from collector %s.", m.Name(), m.Collector()))
 	} else {
-		if m.metricVec == nil {
-			fmt.Println("[DEBUG] Detected metric instead of metric vector.")	
-		} else {
-			fmt.Println(fmt.Sprintf("[DEBUG] Failed to delete metric %s from collector %s.", m.Name(), m.Collector()))	
-		}
+		fmt.Println(fmt.Sprintf("[DEBUG] Failed to delete metric %s from collector %s.", m.Name(), m.Collector()))	
+	
 	}
 	return nil
 }
