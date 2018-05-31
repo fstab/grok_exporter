f6a5e5265584a0b1b58bf629bd4cff04aaefd7cf
Fabian Stäber <fabian@fstab.de>
2018-05-31 22:20:59 +0200

    update year in copyright notice

--- grok_exporter.go
+++ grok_exporter.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2017 The grok_exporter Authors
+// Copyright 2016-2018 The grok_exporter Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
