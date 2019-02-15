488aee952a0943afe1fbac160d981f8ff1d97476
Fabian Stäber <fabian@fstab.de>
2019-02-15 23:17:35 +0100

    #5 continue unifying common fswatcher functionality

--- tailer/fswatcher/fswatcher.go
+++ tailer/fswatcher/fswatcher.go
@@ -181,3 +181,96 @@ func (w *watcher) syncFilesInDir(dir *Dir, readall bool, log logrus.FieldLogger)
 	w.watchedFiles = watchedFilesAfter
 	return nil
 }
+
+func (w *watcher) readNewLines(file *fileWithReader, log logrus.FieldLogger) Error {
+	var (
+		line string
+		eof  bool
+		err  error
+	)
+	for {
+		line, eof, err = file.reader.ReadLine(file.file)
+		if err != nil {
+			return NewErrorf(NotSpecified, err, "%v: read() failed", file.file.Name())
+		}
+		if eof {
+			return nil
+		}
+		log.Debugf("read line %q", line)
+		select {
+		case <-w.done:
+			return nil
+		case w.lines <- Line{Line: line, File: file.file.Name()}:
+		}
+	}
+}
+
+func (w *watcher) checkMissingFile() Error {
+OUTER:
+	for _, g := range w.globs {
+		for watchedFileName, _ := range w.watchedFiles {
+			if g.Match(watchedFileName) {
+				continue OUTER
+			}
+		}
+		// Error message must be phrased so that it makes sense for globs,
+		// but also if g is a plain path without wildcards.
+		return NewErrorf(FileNotFound, nil, "%v: no such file", g)
+	}
+	return nil
+}
+
+// Gets the directory paths from the glob expressions,
+// and makes sure these directories exist.
+func uniqueDirs(globs []glob.Glob) ([]string, Error) {
+	var (
+		result  = make([]string, 0, len(globs))
+		g       glob.Glob
+		dirInfo os.FileInfo
+		err     error
+	)
+	for _, g = range globs {
+		if containsString(result, g.Dir()) {
+			continue
+		}
+		dirInfo, err = os.Stat(g.Dir())
+		if err != nil {
+			if os.IsNotExist(err) {
+				return nil, NewErrorf(DirectoryNotFound, nil, "%q: no such directory", g.Dir())
+			}
+			return nil, NewErrorf(NotSpecified, err, "%q: stat() failed", g.Dir())
+		}
+		if !dirInfo.IsDir() {
+			return nil, NewErrorf(NotSpecified, nil, "%q is not a directory", g.Dir())
+		}
+		result = append(result, g.Dir())
+	}
+	return result, nil
+}
+
+func anyGlobMatches(globs []glob.Glob, path string) bool {
+	for _, pattern := range globs {
+		if pattern.Match(path) {
+			return true
+		}
+	}
+	return false
+}
+
+func containsString(list []string, s string) bool {
+	for _, existing := range list {
+		if existing == s {
+			return true
+		}
+	}
+	return false
+}
+
+func contains(list map[string]*fileWithReader, f *fileWithReader) bool {
+	for _, existing := range list {
+		if existing == f {
+			return true
+		}
+	}
+	return false
+}
