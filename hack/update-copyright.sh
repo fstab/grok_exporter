b6c6a0632795f9ba584702da2b9a25e203687def
Fabian Stäber <fabian@fstab.de>
2020-01-12 00:07:59 +0100

    update apache license header

--- hack/update-copyright.sh
+++ hack/update-copyright.sh
@@ -2,5 +2,12 @@
 
 find . -name '*.go' | while read file ; do
 	year=$(git log -1 --pretty="format:%ci" "$file" | cut -f 1 -d -)
-	sed -Ei "s,// Copyright ([0-9]{4})-[0-9]{4} ,// Copyright \\1-$year ," "$file"
+	sed -Ei "s,// Copyright ([0-9]{4})-[0-9]{4} ,// Copyright \\1-$year ," "$file" # range of years, like 2016-2020
+	sed -Ei "/^\/\/ Copyright $year /! s,// Copyright ([0-9]{4}) ,// Copyright \\1-$year ," "$file" # single year but not current, like 2019
+done
+
+find . -type f -name '*.go' | while read file ; do
+	if ! grep '// Copyright ' $file > /dev/null 2>&1 ; then
+		echo "$file: missing copyright" >&2
+	fi
 done
--- tailer/fswatcher/pollingFseventProducerLoop.go
+++ tailer/fswatcher/pollingFseventProducerLoop.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The grok_exporter Authors
+// Copyright 2019-2020 The grok_exporter Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
